959544aaf792c123dade0c77e9fd740ba3b69e70
omrfrkcpr <omerrfarukcapur@gmail.com>
2023-12-20 10:59:18 +0100

    add app.js

---
+++ JS/01-Intro&Variable/app.js
@@ -0,0 +1,23 @@
+console.log("I'm in app.js");
+
+console.clear();
+
+console.table([
+  { name: "Harvey", age: 35 },
+  { name: "Helen", age: 45 },
+]);
+
+// document.write
+
+document.write("<b>Result on Browser</b> <br>");
+document.write("this is second line <br>");
+
+a = 15;
+b = 45;
+sum = a + b;
+document.write("Sum: ", sum);
+console.log("Sum:", sum);
+
+// PROMPT
+
+prompt("Enter your name");
