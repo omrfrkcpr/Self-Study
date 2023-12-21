24b83a7a913715f563868077c250ebcbca1a9556
omrfrkcpr <omerrfarukcapur@gmail.com>
2023-12-21 10:29:28 +0100

    data types

---
+++ JS/02-Data_Types/data-types.js
@@ -0,0 +1,35 @@
+/* ---------------------------------- */
+/*             DATA TYPES             */
+/* ---------------------------------- */
+
+let sayi = 45;
+console.log(typeof sayi); //result number
+
+//! PRIMITIVE DATA TYPES
+//sie haben einen einzigen Wert.
+//Number
+//string
+//null
+//undefined
+//boolean
+//bigInt
+
+const pi = Math.PI;
+console.log(pi);
+
+let r1 = 6;
+let result1 = 2 * pi * r1;
+console.log(result1);
+
+let r2 = 6;
+let result2 = pi * r2 * r2;
+console.log(result2);
+
+let sayi3 = 113.704;
+console.log(Math.round(sayi3));
+
+let sayi4 = 45.001;
+console.log(Math.ceil(sayi4));
+
+let sayi5 = 37.6;
+console.log(Math.floor(sayi5));
