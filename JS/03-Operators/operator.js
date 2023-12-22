08584fbb291ce2a877273ce745316a48c41772fa
omrfrkcpr <omerrfarukcapur@gmail.com>
2023-12-22 11:32:41 +0100

    add images

---
+++ JS/03-Operators/operator.js
@@ -0,0 +1,22 @@
+/* ---------------------------------- */
+/*              Operators             */
+/* ---------------------------------- */
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+let a = 10;
+let b = 2;
+
+console.log(a + b); // 12 (addition operator)
+console.log(a - b); // 8 (subtraction operator)
+console.log(a / b); // 5 (division operator)
+console.log(a * b); // 20 (multiplication operator)
+console.log(a ** b); // 100 (exponentiation operator)
+console.log(a % b); // 0 (remainder operator)
+
+//^ Modulus - Remainder Operator
+
+let c = 3;
+
+console.log(a / c); // 3.333333333
+console.log(a % c); // 1 (tek sayi)
+console.log(a % b); // 0 (cift sayi)
