5c440e21a34ad4f7183f6c6b3a2023efea220a2d
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-03 10:02:35 +0100

    add 3 print options example

--- JS/04-Conditionals/conditionals.js
+++ JS/04-Conditionals/conditionals.js
@@ -9,16 +9,17 @@
 
 //? Prompt fonskiyonu ile console ortamında kullanıcıdan veri almak mümkündür.
 console.log("-------------------------------------");
-console.log("==IF-ELSE IF== 4 Islem Hesap Makinesi");
+console.log("==IF-ELSE IF== Calculator");
 console.log("-------------------------------------");
 
 //!propmt a girilen değerler string olarak algılanır. sayı olsun istiyorsak önüne ya Number yazmalıyız ya da + koymalıyız
+//?Tek satir (1 durum) bir if-else icin { } koymak zorunda degiliz.
+
 const num1 = +prompt("Enter your first number: ");
 const operator = prompt("Select an operator between +,-,*,/");
 const num2 = Number(prompt("Enter your second number: "));
 let result;
 
-// Tek satir (1 durum) bir if-else icin { } koymak zorunda degiliz.
 if (operator == "+") {
   result = num1 + num2;
 } else if (operator == "-") {
@@ -26,8 +27,21 @@ if (operator == "+") {
 } else if (operator == "*") {
   result = num1 * num2;
 } else if (operator == "/") {
-  result = num1 / num2;
+  if (num2 != 0) result = num1 / num2;
+  else alert("Please enter another number. You can not enter 0");
 } else {
   alert("Wrong operator!");
 }
-console.log(result);
+
+//! 1.Print
+if (result != "undefined") {
+  console.log(result);
+}
+//! 2.Print -- Best
+if (result) {
+  // Deger ise zaten true olarak alir
+  console.log(result);
+}
+//! 3.Print
+//Ikisi de true ise yazdirir
+result && console.log(result);
