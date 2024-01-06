d425242241b3a6576fd9c5b235f3bfa5afd419ea
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-06 23:49:15 +0100

    create challange11

---
+++ JS/Assignments/code-challanges/challange11_fibonacci.js
@@ -0,0 +1,6 @@
+//! Write a JavaScript program that computes and prints the first n Fibonacci numbers without using any built-in functions (like Math.pow, Math.sqrt, Array methods, etc.) or arrays.
+
+//! - You must not use any built-in JavaScript functions related to Fibonacci sequences or arrays.
+//! - The program should prompt the user for the value of n.
+//! - Display the first n Fibonacci numbers.
+//! Example: For n = 7, the Fibonacci sequence would be: 0, 1, 1, 2, 3, 5, 8
