0bfda3275ac3afc4c4f84bc085bdf01c70032d4e
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-07 00:10:14 +0100

    create challange12 js

---
+++ JS/Assignments/code-challanges/challange12_GCD.js
@@ -0,0 +1,5 @@
+//! Write a JavaScript program that calculates and prints the Greatest Common Divisor (GCD) of two given numbers using only conditional statements (if, else if, else) and loops (for, while).
+
+//! - You can only use conditional statements (if, else if, else) and loops (for, while, do while).
+//! - The program should prompt the user for two numbers, a and b.
+//! - Display the calculated GCD on the console.
