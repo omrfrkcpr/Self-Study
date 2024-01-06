7aaab5f5add6821538e290d40118cbcf6eaa586c
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-07 00:45:38 +0100

    create challange13 Armstrong number checker

---
+++ JS/Assignments/code-challanges/challange13_ArmstrongNumberChecker.js
@@ -0,0 +1,6 @@
+//! Write a JavaScript program to check if a given number is an Armstrong number using only conditional statements (if, else if, else) and loops (for, while).
+
+//! - An Armstrong number (also known as narcissistic number) of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself.
+//! - You can only use conditional statements (if, else if, else) and loops (for, while, do while).
+//! - The program should prompt the user for a number.
+//! - Display whether the entered number is an Armstrong number or not.
