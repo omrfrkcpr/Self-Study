2a5f4e390a32774d4888a0dc966befcac7c3a71b
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-10 11:39:37 +0100

    add challange19 - login

---
+++ JS/Assignments/code-challanges/challange19_Login.js
@@ -0,0 +1,24 @@
+//!
+
+const user = "Mark";
+const psswd = "1234";
+let remain = 3;
+
+while (remain > 0) {
+  let userName = prompt("Please enter your username");
+  let userPassword = +prompt("Please enter your password");
+  remain--;
+
+  if (userName == user && userPassword == psswd) {
+    console.log(`You are succesfully logged in 🥳`);
+    break;
+  } else if (userName == user && userPassword != psswd) {
+    console.log(`Your password is wrong. ${remain} left`);
+  } else if (userName != user && userPassword == psswd) {
+    console.log(`Your username is wrong. ${remain} left`);
+  } else console.log(`Your username and password are wrong. ${remain} left`);
+
+  if (remain == 0) {
+    console.log("Sorry. You cannot log in 😞");
+  }
+}
