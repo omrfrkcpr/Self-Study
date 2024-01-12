fd3a6bf795d8211d7dba271bb8308b216974068f
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-12 11:53:14 +0100

    add scope example (var)

--- JS/06-Functions/scope.js
+++ JS/06-Functions/scope.js
@@ -31,3 +31,53 @@ scopeFunction();
 console.log(second); // 12
 console.log(first); // 8
 console.log(scopeFunction(4)); // 1234
+
+// ? *********************************************************
+
+//! global scope
+
+let num1 = 11;
+
+const func1 = () => {
+  let num1 = 44; //! function-scope
+
+  num1++;
+  return num1;
+};
+
+console.log(func1()); // 45
+
+// num1 icin global ve function scope degerleri
+console.log(num1); // 11
+num1 = func1(); // globalscope = functionscope
+console.log(num1); // 45
+
+{
+  num1++; // 46
+  let x = 5;
+  x++; // 6
+  let y = x + 15; //21
+  console.log(y); //? blocked-scope 21
+}
+
+console.log(num1); // 46
+// console.log(y); // ReferenceError: y is not defined
+
+// ? ************************ VAR ***************************
+
+num2 = 100;
+
+var num2; //? hoisted (wird hochgezogen)
+console.log(num2);
+
+var result = "Hallo";
+
+if (num2 == 100) {
+  let result = "Hi";
+  console.log(result); //? local(blocked)
+} else {
+  result = "Salut";
+  console.log(result); //? global
+}
+
+console.log(result); //? global
