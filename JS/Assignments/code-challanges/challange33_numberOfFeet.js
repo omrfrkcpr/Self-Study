42ef31ef395fcba98ce9c0423004e8f297cc0934
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-15 10:43:01 +0100

    add challange33 - numberOfFeet

---
+++ JS/Assignments/code-challanges/challange33_numberOfFeet.js
@@ -0,0 +1,10 @@
+//! Write a function that takes the number of animals on the farm as parameters and returns the sum of the number of feet.
+
+//! Chicken, Cow, Duck, Goat
+
+const numberOfFeet = (chicken, cow, duck, goat) => {
+  let sum = (chicken + duck) * 2 + (cow + goat) * 4;
+  console.log(sum);
+};
+
+numberOfFeet(5, 3, 2, 10); // 66
