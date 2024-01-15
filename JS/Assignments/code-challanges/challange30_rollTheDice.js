b34bff726ce05aa6d7d1785b625eeea5efcb4559
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-15 09:47:54 +0100

    add extra

--- JS/Assignments/code-challanges/challange30_rollTheDice.js
+++ JS/Assignments/code-challanges/challange30_rollTheDice.js
@@ -6,3 +6,22 @@ const rollTheDice = () => {
 };
 
 rollTheDice();
+
+//& Extra - Write a function that prints the values of the dice thrown as small and large each time they are rolled and says congratulations when there is a tie.
+
+const diceRolling = () => {
+  const dice = () => Math.floor(Math.random() * 6) + 1;
+
+  let dice1 = dice();
+  let dice2 = dice();
+
+  if (dice1 == dice2) {
+    console.log(`Congratulations 🥳. You rolled ${dice1}-${dice2}!`);
+  } else {
+    dice1 > dice2
+      ? console.log(`You rolled ${dice2}-${dice1}!`)
+      : console.log(`You rolled ${dice1}-${dice2}!`);
+  }
+};
+
+diceRolling();
