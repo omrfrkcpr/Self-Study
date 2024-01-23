de64030a5df33b78d77811d4f5e05bed5388d60b
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-01-23 10:57:16 +0100

    add loop over in objects with using for-of and for-in (+built-in methods)

--- JS/09-Objects/object.js
+++ JS/09-Objects/object.js
@@ -79,3 +79,118 @@ console.log(personal); /* {
     phone: '555-34-34'
   }
 */
+
+/* ---------------------------------------------------------- */
+//*                      Object Methods                       */
+/* ---------------------------------------------------------- */
+
+//^ Nested Objects
+
+let employeesList = {
+  person1: {
+    name: "Abdulkadir",
+    lastName: "BAKI",
+    datOfBirth: 1980,
+    salary: 20000,
+    job: "developer",
+  },
+  person2: {
+    name: "Elif",
+    lastName: "AKALIN",
+    datOfBirth: 1990,
+    salary: 18000,
+    job: "tester",
+  },
+  person3: {
+    name: "Esra",
+    lastName: "BILGIN",
+    datOfBirth: 1985,
+    salary: 15000,
+    job: "devops",
+  },
+};
+
+console.log(employeesList.person3.salary); // 15000
+console.log(employeesList.person2.name); // Elif
+console.log(employeesList["person1"].job); // developer
+
+//==============================================================//
+
+//^ for of = is used in sequential arrays, but for-of is not used in objects because they have a more complex structure. thats why we use object built-in methods
+
+let numbersAndLetters = ["a", 4, 6, 7, "C"];
+for (const i of numbersAndLetters) {
+  console.log(i); /* a
+  4
+  6
+  7
+  C */
+}
+/*
+for (const i of employeesList) {
+    console.log(i); //* Uncaught TypeError: employeeList is not iterable!
+}
+*/
+
+for (const key of Object.keys(employeesList)) {
+  console.log(key); /* person1
+  person2
+  person3 */
+}
+
+for (const value of Object.values(employeesList)) {
+  console.log(Object.values(value)[0]); /* Abdulkadir
+  Elif
+  Esra */
+}
+
+//^ for in = it can be used in objects.
+
+let numbersAndLetters2 = ["a", 4, 6, 7, "C"];
+for (const i in numbersAndLetters2) {
+  console.log(i); /* 0
+  1
+  2
+  3
+  4 */
+}
+
+for (const i in employeesList) {
+  console.log("with using for-in method :", i, employeesList[i].job);
+  /*
+  with using for in method : person1 developer
+  with using for in method : person2 tester
+  with using for in method : person3 devops
+  */
+}
+
+//=============================================================//
+
+//^ Built-In Methods in Object
+//^ Object.keys() / Object.values() / Object.entries()
+//? converts object to array (now we can loop over in our object)
+
+console.log(Object.keys(employeesList)); // [ 'person1', 'person2', 'person3' ]
+console.log(Object.values(employeesList)); /* [
+    {
+      name: 'Abdulkadir',
+      lastName: 'BAKI',
+      datOfBirth: 1980,
+      salary: 20000,
+      job: 'developer'
+    },
+    {
+      name: 'Elif',
+      lastName: 'AKALIN',
+      datOfBirth: 1990,
+      salary: 18000,
+      job: 'tester'
+    },
+    {
+      name: 'Esra',
+      lastName: 'BILGIN',
+      datOfBirth: 1985,
+      salary: 15000,
+      job: 'devops'
+    }
+  ] */
