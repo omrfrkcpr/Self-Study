2e25dcb1f3cff5e6b9cd71b1c697bd1fc9f408b4
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-14 11:46:37 +0100

    bugs detected

--- JS/assignments/04_country_foods/app.js
+++ JS/assignments/04_country_foods/app.js
@@ -1,28 +1,35 @@
-let showList = []; // to catch the data comes from fetch
+let showList = []; //!boş bir array aç. alttaki arama olayında datayı görebilmek için global e boş bir array açıp, fetch ile gelen data yı bu boş array e attık
 
+//!ilk ekrandaki resimleri api den çekiyoruz (search meal by name)
+
+//www.themealdb.com/
 fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
   .then((resp) => resp.json())
   .then((data) => {
     showList = data;
-    console.log(showList);
+
     showScreen(showList.meals);
   })
   .catch((err) => console.log(err));
 
-const showScreen = (data) => {
-  const foodDiv = document.querySelector(".food");
-  foodDiv.innerHTML = "";
-  data.forEach((meal) => {
-    foodDiv.innerHTML += `
-        <div class="col-md-3 m-1 text-center m-auto mt-5 d-flex flex-column align-items-center justify-content-center">
-            <p class="fw-bold border-bottom w-75 text-center">${meal.strMeal}</p>
-            <img src=${meal.strMealThumb} style="width:250px; border-radius:10px"/>
-        </div>
-        `;
-  });
-};
+//!EKRANA BASTIRMA
+function showScreen(data) {
+  const mealsDiv = document.querySelector(".food");
+  //!ekranı temizle, hangi dizi geldiyse o basılabilsin
+  mealsDiv.innerHTML = "";
 
-// when click on country flag, get that country's meals
+  data.forEach((a) => {
+    mealsDiv.innerHTML += `
+ <div class="col-md-3 m-1 text-center m-auto mt-5 d-flex flex-column align-items-center justify-content-center">
+ <p class="fw-bold border-bottom w-75 text-center">${a.strMeal}</p>
+ <img src=${a.strMealThumb} style="width:250px; border-radius:10px"/>
+</div>
+
+`;
+  });
+}
+
+//! bayraklara tıklanınca o bayrağın yemekleri gelsin
 document.querySelectorAll("img").forEach(
   (country) =>
     (country.onclick = () => {
@@ -34,23 +41,16 @@ document.querySelectorAll("img").forEach(
     })
 );
 
-// oninput
-document.querySelector("#search").oninput = (e) => {
-  let newData = showList.meals.filter((a) => {
-    a.strMeal.toLowerCase().includes(e.target.value.toLowerCase());
-  });
-  console.log(newData);
-  showScreen(newData);
+//!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
+
+//? e.target.value=> inputa her veri girişindeki değerleri alabilmek için kullanılır, mesela onclick te sadece e.value kullanılır, çünkü inputla işimiz bitince onclick ile inputtaki veriyi topla git
+
+document.querySelector("#ara").oninput = (e) => {
+  //   document.querySelector("#ara").value;
+  // console.log(e.target.value);
+  let veri = showList.meals.filter((a) =>
+    a.strMeal.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+
+  showScreen(veri);
 };
-
-// //!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
-
-// //? e.target.value=> inputa her veri girişindeki değerleri alabilmek için kullanılır, mesela onclick te sadece e.value kullanılır, çünkü inputla işimiz bitince onclick ile inputtaki veriyi topla git
-
-// document.querySelector("#ara").oninput=(e)=>{
-// //   document.querySelector("#ara").value;
-// // console.log(e.target.value);
-// let veri=  showList.meals.filter((a)=>a.strMeal.toLowerCase().includes(e.target.value.toLowerCase()))
-
-// showScreen(veri)
-// }
