069ac2f0f281a5276dc7ef2fe5f14e26522a62de
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-14 09:32:19 +0100

    asyn_2 - finish fetch try/catch throw methods

--- JS/15-Async_2/app.js
+++ JS/15-Async_2/app.js
@@ -3,23 +3,55 @@ const defaultImage =
 
 const getNews = async () => {
   const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78e";
-  const fetchAPI = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  );
 
-  const data = await fetchAPI.json();
+  try {
+    const fetchAPI = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    );
 
-  console.log(data.articles); // for testing !!!
-  renderNews(data.articles);
+    //? error handling
+    if (!fetchAPI.ok) {
+      throw new Error("News can not be fetched");
+    }
+    const data = await fetchAPI.json();
+
+    console.log(data.articles); // for testing !!!
+    renderNews(data.articles);
+  } catch (error) {
+    console.log(error);
+    renderError(error);
+  }
+};
+
+const renderError = (err) => {
+  document.querySelector("#news").innerHTML = `
+    <h1>${err}</h1>
+    <img src="./404.png"/>
+    `;
 };
 
 getNews();
 
 const renderNews = (news) => {
   news.forEach((item) => {
-    const { urlToImage } = item; //~ destructuring
+    const { urlToImage, title, content, url } = item; //~ destructuring
     document.querySelector("#news").innerHTML += `
-            <img src="${urlToImage ? urlToImage : defaultImage}"/>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+
+    <div class="card" style="width: 18rem;">
+      <img src="${
+        urlToImage ? urlToImage : defaultImage
+      }" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${content ? content : ""}</p>
+        <a href="${url}" target="_blank" class="btn btn-primary">Details</a>
+      </div>
+    </div>
+    
+    
+    </div>
+    
         `;
   });
 };
