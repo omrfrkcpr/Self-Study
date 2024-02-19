d53eabbc958a5eccda7857f33f9a1988f8511c4f
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-19 23:32:23 +0100

    add html and script codes (finished project)

---
+++ JS/assignments/06_digital_clock/script.js
@@ -0,0 +1,22 @@
+function updateTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  document.getElementById("ampm").textContent = ampm;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
