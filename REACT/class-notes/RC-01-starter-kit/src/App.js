e13cc91edd69fe91a94d8de76362615ac8483ec2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-20 11:42:09 +0100

    initial

---
+++ REACT/class-notes/RC-01-starter-kit/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  //? it should return only one div. But inside it it can be lots of divs !
+  return <div>App</div>;
+};
+
+export default App;
+
+//* Initial function
+// rfce // expression
+// rafce // arrow
