750062c961e618fad3983795feddf3f29dbab32b
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-21 15:38:42 +0100

    fix bug buttons

--- JS/assignments/07_stop_watch/script.js
+++ JS/assignments/07_stop_watch/script.js
@@ -1,17 +1,23 @@
-let stopwatchInterval,
-  timer = 0;
+let timer = 0;
+let stopwatchInterval;
 
 const startStopwatch = () => {
-  stopwatchInterval = setInterval(showStopwatch, 10);
+  if (!stopwatchInterval) {
+    stopwatchInterval = setInterval(showStopwatch, 10);
+  }
 };
+
 const pauseStopwatch = () => {
   clearInterval(stopwatchInterval);
+  stopwatchInterval = null;
 };
+
 const resetStopwatch = () => {
   pauseStopwatch();
   timer = 0;
-  document.querySelector(".time").textContent = `00: 00: 00`;
+  document.querySelector(".time").textContent = "00 : 00 : 00";
 };
+
 const showStopwatch = () => {
   const minute = Math.floor(timer / 100 / 60);
   const second = Math.floor((timer / 100) % 60);
@@ -19,8 +25,8 @@ const showStopwatch = () => {
   timer++;
 
   document.querySelector(".time").textContent = `
-    ${minute.toString().padStart(2, "0")}:
-    ${second.toString().padStart(2, "0")}:
-    ${msecond.toString().padStart(2, "0")}
+    ${minute.toString().padStart(2, "0")} : 
+    ${second.toString().padStart(2, "0")} : 
+    ${msecond.toString().padStart(2, "0")} 
     `;
 };
