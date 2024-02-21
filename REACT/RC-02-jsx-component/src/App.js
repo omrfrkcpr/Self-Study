479bfde4c01b55bd18e2a05fa328547410e40308
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-21 09:54:27 +0100

    add Footer.jsx

--- REACT/RC-02-jsx-component/src/App.js
+++ REACT/RC-02-jsx-component/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import React from "react";
+import React from "react"; // only in this file necessary
 import Msg from "./msg/Msg";
+import Footer from "./footer/Footer";
 
 const App = () => {
   //? it should return only one div. But inside it it can be lots of divs !
   return (
     <div>
       <Msg />
+      <Footer />
     </div>
   );
 };
