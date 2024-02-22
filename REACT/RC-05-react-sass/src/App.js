b1cb0af982065f9f7235abd548af6551501e3704
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-22 11:13:02 +0100

    chore: edit folder-file skeleton (initial)

--- REACT/RC-05-react-sass/src/App.js
+++ REACT/RC-05-react-sass/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 
 const App = () => {
---
+++ REACT/RC-05-react-sass/src/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: "Ayşe Yilmaz",
+    job: "Developer",
+    comment: "Uygulamayı çok beğendim...",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+  {
+    id: 2,
+    name: "Haluk Engin",
+    job: "Tester",
+    comment: "Uygulama çok zor.",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 3,
+    name: "Bensu Can",
+    job: "Grafiker",
+    comment: "Tebrikler çok başarılı",
+    img: "https://images.unsplash.com/photo-1610564558732-a36a4e28151c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydGFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    name: "Necip Baki",
+    job: "Stajer",
+    comment: "Geliştirilmesi gerekiyor.",
+    img: "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 5,
+    name: "Gül Daim",
+    job: "Data Scientist",
+    comment: "Daha hızlı olmalı",
+    img: "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D",
+  },
+];
+
+export default data;
