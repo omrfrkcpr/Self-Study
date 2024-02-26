ebd1be8b5bc0c8fa5d8112e781c8eb4b6c612dfa
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-26 12:55:57 +0100

    add responsivity (/w mixins)

--- REACT/Projects/horoscope_page/src/components/main/Card.jsx
+++ REACT/Projects/horoscope_page/src/components/main/Card.jsx
@@ -21,3 +21,24 @@ const Card = ({ title, date, desc, image }) => {
 };
 
 export default Card;
+
+// return (
+//   <div>
+//     {date.includes("February") && (
+//       <div className="cards">
+//         <div className="cards--title">
+//           <h1>{title}</h1>
+//         </div>
+//         <div className="cards--date">
+//           <h1>{date}</h1>
+//         </div>
+//         <div className="cards--img">
+//           <img src={image} alt="" />
+//         </div>
+//         <div className="cards--desc">
+//           <p>{desc}</p>
+//         </div>
+//       </div>
+//     )}
+//   </div>
+// );
