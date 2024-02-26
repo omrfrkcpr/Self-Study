f3a2f64af07897e238086aa26c67ddf68cecec24
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-26 10:03:03 +0100

    navbar - finish structure and style

--- REACT/Projects/horoscope_page/src/components/navbar/Navbar.jsx
+++ REACT/Projects/horoscope_page/src/components/navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Navbar.scss";
+import logo from "../../helper/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar--logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="navbar--links">
+        <a href="#horoscope">Horoscope</a>
+        <a href="#daily">Daily</a>
+        <a href="#tarot">Tarot</a>
+        <a href="#article">Article</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
