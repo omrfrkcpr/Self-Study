774c2d9063c4bdb81cc200b36ffd6dd188acff70
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-26 11:20:12 +0100

    chore: add main and card components

--- REACT/Projects/horoscope_page/src/components/main/Card.jsx
+++ REACT/Projects/horoscope_page/src/components/main/Card.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ title, date, desc, image }) => {
+  // console.log(item);
+  return (
+    <div className="cards">
+      <div className="cards--title">
+        <h1>{title}</h1>
+      </div>
+      <div className="cards--date">
+        <h1>{date}</h1>
+      </div>
+      <div className="cards--img">
+        <img src={image} alt="" />
+      </div>
+      <div className="cards--desc">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- REACT/Projects/horoscope_page/src/components/main/Main.jsx
+++ REACT/Projects/horoscope_page/src/components/main/Main.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
 
 export const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="card-container">
+      {data.map((item) => (
+        <Card {...item} key={item.id} />
+      ))}
+    </div>
+  );
 };
+
+// eger obje yollaniyorsa, spread operator kullanilabilir
