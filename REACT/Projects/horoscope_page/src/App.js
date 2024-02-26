23d1b6c6987f084118d3cacd2582f78082ee1af5
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-26 09:42:06 +0100

    add helper and components folders

--- REACT/Projects/horoscope_page/src/App.js
+++ REACT/Projects/horoscope_page/src/App.js
@@ -1,9 +1,18 @@
 import "./App.scss";
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import { Main } from "./components/main/Main";
 
 const App = () => {
   //? it should return only one div. But inside it it can be lots of divs !
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
