9175288f20c0dc523a90c2da07fda88b65b5d664
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-26 10:45:57 +0100

    header - finish structure and style

--- REACT/Projects/horoscope_page/src/components/header/Header.jsx
+++ REACT/Projects/horoscope_page/src/components/header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <span className="header--title">Who are you?</span>
+    </div>
+  );
 };
 
 export default Header;
