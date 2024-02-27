a2ddce1c69cdc602c349947b3ce5558a22ac3868
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-27 09:41:55 +0100

    add comments

--- REACT/RC-06-events-hooks/src/1-events_hooks/Events.jsx
+++ REACT/RC-06-events-hooks/src/1-events_hooks/Events.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+//! react alanında parametreli fonksiyon çağıracaksak, fonksiyonun adının önüne ()=> işaretini koymalıyız, yoksa fonksiyonu event ı beklemeden çalıştırıyor
+
 const Events = () => {
+  // JS field
   let title = "Hello";
   let count = 0;
 
@@ -10,21 +13,47 @@ const Events = () => {
     document.querySelector("span").textContent = count;
   };
 
-  const changeTitle = () => {};
+  const changeTitle = (a) => {
+    console.log(a);
+    document.querySelector(".title-span").textContent = a;
+  };
+
+  //? Bir componentin return kismi 3 farkli yontem ile yeniden render edilir.
+  //? 1- Herhangi bir state degisirse
+  //? 2- Props degisirse
+  //? 3- Refresh force edilirse (tercih edilen bir durum olmaz)
 
   return (
+    // JSX field
     <div className="container text-center mt-4">
-      <h1>Title: {title}</h1>
+      <h1>
+        Title: <span className="title-span">{title}</span>
+      </h1>
       <h2>
         COUNT: <span className="text-danger">{count}</span>
       </h2>
       <button onClick={increase} className="btn btn-primary m-1">
         Increase
       </button>
-      <button className="btn btn-danger m-1">Change Title</button>
+      <button
+        onClick={() => changeTitle("There!")}
+        className="btn btn-danger m-1"
+      >
+        Change Title
+      </button>
       <button className="btn btn-info m-1">Clicked!</button>
     </div>
   );
 };
+
+//?--------------------------------------------------
+//! Konsolda, güncellenen değişen count u görebiliriz ancak web sayfasında (biz görüntüle demeden) görüntülenmiyor..
+//* Çünkü, REACT herhangi bir element i default olarak static sayar
+//*DOM manipülasyonunu en aza indirmek için bunu yapar
+//* Hangi elementin interactive olduğu konusunda React'e bilgi vermeliyiz.
+//! REACT'i interactive elements hakkında bilgilendirmek için iki yöntem kullanıyoruz
+//* 1. Statefull Classes. State-based inform (Class Components)
+//* 2. Hooks (Functional Components)
+//?-------------------------------------------------
 
 export default Events;
