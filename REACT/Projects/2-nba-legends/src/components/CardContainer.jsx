9887d8abd0af18c46f47bb14df0c4883cb6a06b3
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-01 11:23:21 +0100

    add player filter functionality

--- REACT/Projects/2-nba-legends/src/components/CardContainer.jsx
+++ REACT/Projects/2-nba-legends/src/components/CardContainer.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../helpers/data";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
+
+  //! Be Careful! data is not changable, but dataFilter is changable
+  const dataFilter = data.filter((a) => a.name.includes(search));
+
   return (
-    <div>
+    <>
       <Form.Control
         type="search"
         placeholder="Search a player..."
-        className="mb-4"
+        className="mb-4 w-75 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
       />
       <Container className="card-container rounded-4">
         <Row className="g-4">
-          {data.map((player, index) => {
+          {dataFilter.map((player, index) => {
             return (
               <Col md={6} lg={6} xl={4} xxl={3} className="text-center">
                 <PlayerCard {...player} key={index} />
@@ -22,7 +28,7 @@ const CardContainer = () => {
           })}
         </Row>
       </Container>
-    </div>
+    </>
   );
 };
 
