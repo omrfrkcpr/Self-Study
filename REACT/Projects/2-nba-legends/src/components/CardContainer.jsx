7e6690555ddfae68fafab0323d351393c988cc08
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-01 11:04:35 +0100

    CardContainer + PlayerCard - style

--- REACT/Projects/2-nba-legends/src/components/CardContainer.jsx
+++ REACT/Projects/2-nba-legends/src/components/CardContainer.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { data } from "../helpers/data";
-import { Col, Form, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
   return (
     <div>
-      <Form.Control type="search" placeholder="Search a player..." />
-      <Row>
-        {data.map((player, index) => {
-          return (
-            <Col>
-              <PlayerCard {...player} key={index} />
-            </Col>
-          );
-        })}
-      </Row>
+      <Form.Control
+        type="search"
+        placeholder="Search a player..."
+        className="mb-4"
+      />
+      <Container className="card-container rounded-4">
+        <Row className="g-4">
+          {data.map((player, index) => {
+            return (
+              <Col md={6} lg={6} xl={4} xxl={3} className="text-center">
+                <PlayerCard {...player} key={index} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 };
--- REACT/Projects/2-nba-legends/src/components/PlayerCard.jsx
+++ REACT/Projects/2-nba-legends/src/components/PlayerCard.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const PlayerCard = ({ img, name, statistics }) => {
+  const [toggleImg, setToggleImg] = useState(true);
+
   return (
     <div>
-      <Card>
-        <Card.Img variant="top" src={img} />
+      <Card
+        className="player-card m-auto"
+        onClick={() => setToggleImg(!toggleImg)}
+      >
+        {toggleImg ? (
+          <Card.Img className="player-logo" variant="top" src={img} />
+        ) : (
+          <ul className="m-auto">
+            {statistics.map((item, i) => (
+              <li key={i} className="h5 text-start list-unstyled">
+                {i === 0 ? "🏀" : i === 1 ? "🌸" : i === 2 ? "🎉" : "💯"}
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <Card.Footer>
+          <Card.Title>{name}</Card.Title>
+        </Card.Footer>
       </Card>
     </div>
   );
