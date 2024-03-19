69192f34f85501b1635b876923dd620baf4fd378
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-19 09:41:47 +0100

    add navbar links

--- REACT/Projects/6-shopping-cart/src/components/Navbar.jsx
+++ REACT/Projects/6-shopping-cart/src/components/Navbar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-import {NavLink} from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-   <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg bg-secondary">
+      <div className="container-fluid">
+        <span className="navbar-brand text-white">Shopping Cart</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link active" aria-current="page">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/new-product" className="nav-link">
+                New Product
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/product-list" className="nav-link">
+                Product List
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
