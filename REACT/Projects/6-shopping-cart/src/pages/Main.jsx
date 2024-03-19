c8a063f6023bfaba1a3aece77378ffca5e2cb2d2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-19 12:09:17 +0100

    chore: navigate buttons -useNavigate() hook

--- REACT/Projects/6-shopping-cart/src/pages/Main.jsx
+++ REACT/Projects/6-shopping-cart/src/pages/Main.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-
+  const navigate = useNavigate();
   return (
     <main className="main">
       <div>
         <h1>Welcome to Shopping Cart Application</h1>
         <div className="main-buttons">
           <button
+            onClick={() => navigate("/new-product")}
             className="btn btn-outline-primary px-4"
-          
           >
             Add New Product
           </button>
           <button
+            onClick={() => navigate("/product-list")}
             className="btn btn-primary px-4 ms-3"
-           
           >
             See Products
           </button>
