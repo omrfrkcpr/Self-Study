220d6fa46452cf4f56f859ae4fca1703448192c3
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-26 11:19:40 +0100

    feat: conditional rendering based on existence of recipe

--- REACT/Projects/08-recipe-app/src/pages/home/Home.jsx
+++ REACT/Projects/08-recipe-app/src/pages/home/Home.jsx
@@ -4,25 +4,24 @@ import Header from "../../components/header/Header";
 import RecipeCard from "./RecipeCard";
 import { HomeImg, ImgDiv } from "./HomeStyles";
 import homeSvg from "../../assets/home.svg";
-
-
+import { useContext } from "react";
+import { RecipeContext } from "../../context/RecipeProvider";
 
 const Home = () => {
-
-
+  const { recipes } = useContext(RecipeContext);
   return (
     <div>
       <Header />
 
-    
+      {recipes.length ? (
         <div>
           <RecipeCard />
         </div>
-    
+      ) : (
         <ImgDiv>
           <HomeImg src={homeSvg} alt="" />
         </ImgDiv>
- 
+      )}
     </div>
   );
 };
