41e154e2e9bda09f7507907e73829f2672b0c321
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-28 17:33:14 +0100

    initial tailwind

--- REACT/Projects/10-movie-app/movie-app-inclass/src/pages/Register.jsx
+++ REACT/Projects/10-movie-app/movie-app-inclass/src/pages/Register.jsx
@@ -11,24 +11,52 @@ const Register = () => {
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">First Name</label>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              class="peer"
+              placeholder=" "
+              required
+            />
+            <label htmlFor="firstName">First Name</label>
           </div>
-          <div>
-            <input name="floating_text" type="text" required />
-            <label htmlFor="floating_text">Last Name</label>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              class="peer"
+              placeholder=" "
+              required
+            />
+            <label htmlFor="lastName">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
-            <label htmlFor="floating_email">Email</label>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              name="email"
+              id="email"
+              class="peer"
+              placeholder=" "
+              type="email"
+            />
+            <label htmlFor="email">Email</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
-            <label htmlFor="floating_password">Password</label>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              name="password"
+              id="password"
+              class="peer"
+              placeholder=" "
+              type="password"
+            />
+            <label htmlFor="password">Password</label>
           </div>
-          <button type="submit">Register</button>
-          <button type="button">
+          <button className="btn-danger" type="submit">
+            Register
+          </button>
+          <button className="btn-danger flex justify-between" type="button">
             Continue with Google
             <GoogleIcon color="currentColor" />
           </button>
--- REACT/Projects/10-movie-app/movie-app-inclass/tailwind.config.js
+++ REACT/Projects/10-movie-app/movie-app-inclass/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  //* projemize bir plugin dahil etmek istiyorsak config dosyasına eklememiz gerekir
+  //   content: [
+  //     "./node_modules/flowbite/**/*.js"
+  // ]
+  //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
-    extend: {},
+    //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
+
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
   },
   plugins: [],
+  //* projemize bir plugin dahil etmek istiyorsak config dosyasına eklememiz gerekir
+  //   plugins: [
+  //     require('flowbite/plugin')
+  // ]
+  darkMode: "selector",
 };
