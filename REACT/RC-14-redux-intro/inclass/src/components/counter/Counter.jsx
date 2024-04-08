4d18841142d996f2c009ad68e5a804ac71652ad2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-08 12:14:23 +0200

    actions types

--- REACT/RC-14-redux-intro/inclass/src/components/counter/Counter.jsx
+++ REACT/RC-14-redux-intro/inclass/src/components/counter/Counter.jsx
@@ -1,5 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./Counter.css";
+import {
+  decrement,
+  reset,
+  increment,
+} from "../../redux/actions/counterActions";
 
 const Counter = () => {
   // useSelector(state => state.reducer)
@@ -13,13 +18,15 @@ const Counter = () => {
       <div>
         <button
           className="counter-button negative"
-          onClick={() => dispatch({ type: "DEC" })}
+          // onClick={() => dispatch({ type: DEC })}
+          onClick={() => dispatch(decrement())}
         >
           decrease
         </button>
         <button
           className="counter-button zero"
-          onClick={() => dispatch({ type: "RESET" })}
+          // onClick={() => dispatch({ type: RESET })}
+          onClick={() => dispatch(reset())}
         >
           reset
         </button>
@@ -27,7 +34,8 @@ const Counter = () => {
           className="counter-button positive"
           // onClick={() => dispatch("INC")} //! action obje olmak zorunda o nedenle argüman gönderirken obje olarak göndermek zorundayız. Bu şekilde hata alırız.
           // onClick={() => dispatch({ tip: "INC" })}//? obje içerisinde gönderdiğimiz property type adında olmalı
-          onClick={() => dispatch({ type: "INC" })} //* dispatch içerisinde verdiğimiz argüman reducer içerisindeki action parametresine karşılık gelir.
+          // onClick={() => dispatch({ type: INC })} //* dispatch içerisinde verdiğimiz argüman reducer içerisindeki action parametresine karşılık gelir.
+          onClick={() => dispatch(increment())}
         >
           increase
         </button>
---
+++ REACT/RC-14-redux-intro/inclass/src/redux/actions/counterActions.js
@@ -0,0 +1,17 @@
+import { DEC, INC, RESET } from "../types/counterTypes";
+
+// Action: Bir javascript objesidir. Store'da hangi state'in değişeceği bilgisini(type) ve state verisini taşır(payload).
+
+// Bir action type ve payload olmak üzere iki adet property'den oluşur. type action'un belirteçi hangi işlemin yapıldığını belirtir. payload ise gönderilecek veriyi içeren property'dir.
+//payload yerine gönderdiğiniz verinin adını da (movie, comment vs.) yazabilirsiniz fakat genel bir kural olarak payload property'sini kullanmaya özen göstermelisiniz.
+//* Bir action her zaman javascript objesi olmak zorundadır.
+
+//? Action creater fonksyionlarının tanimlanmsi
+// Action'ları daha kullanışlı yapmak için için çoğu zaman Action Creator'lardan faydalanırız. Action Creator'lar birer arrow function'lardır. Sadece değişen payload kısmını parametre alıp bir action geri döner. Type'lar genelde değişmeyen, sadece yönlendirici sabitler olduğu için ayrı bir dosyada tutulur(actionTypes). Ama artık redux toolkit kullanımı ile birlikte hepsi tek dosyada yapılır.
+
+export const increment = () => {
+  return { type: INC };
+};
+export const decrement = () => ({ type: DEC });
+
+export const reset = () => ({ type: RESET });
