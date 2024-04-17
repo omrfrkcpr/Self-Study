260c1745476fd73569c859800e8eef9ec53e6918
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-17 10:29:21 +0200

    feat: import middleware for serializableCheck

--- REACT/Projects/11-stock-app/src/app/store.jsx
+++ REACT/Projects/11-stock-app/src/app/store.jsx
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "../features/authSlice";
-// import authMidReducer from "../features/authSliceMiddleware"; //!middleware ile olan kullanım
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
   key: "root",
@@ -14,11 +22,15 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
-    // auth:authMidReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
-
 export let persistor = persistStore(store);
 
 export default store;
