a09b3dffcdd2e470fedcacef03c9d07a586984d9
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-17 11:07:54 +0200

    chore: nested components (router)

--- REACT/Projects/11-stock-app/src/pages/Dashboard.jsx
+++ REACT/Projects/11-stock-app/src/pages/Dashboard.jsx
@@ -6,9 +6,10 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import useAuthCall from "../hooks/useAuthCall";
+import { Outlet } from "react-router-dom";
 
 function Dashboard() {
-  const {logout} = useAuthCall()
+  const { logout } = useAuthCall();
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -17,9 +18,14 @@ function Dashboard() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             STOCK APP
           </Typography>
-          <Button color="inherit" onClick={logout}>Logout</Button>
+          <Button color="inherit" onClick={logout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
+      <div style={{ marginTop: "4rem" }}>
+        <Outlet />
+      </div>
     </Box>
   );
 }
--- REACT/Projects/11-stock-app/src/router/AppRouter.jsx
+++ REACT/Projects/11-stock-app/src/router/AppRouter.jsx
@@ -1,23 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import LoginMid from "../pages/LoginMid";
-// import RegisterMid from "../pages/RegisterMid";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../layouts/Home";
+import Firms from "../layouts/Firms";
+import Products from "../layouts/Products";
+import Sales from "../layouts/Sales";
+import Purchases from "../layouts/Purchases";
+import Brands from "../layouts/Brands";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        {/* middlewareli fonksiyonların olduğu sayfalar */}
-        {/* <Route path="/" element={<LoginMid />} />
-        <Route path="register" element={<RegisterMid />} /> */}
         <Route path="/" element={<Login />} />
-        <Route path="register" element={<Register />} />        
+        <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          {/* Nested routes in the stock route  */}
+          <Route path="" element={<Dashboard />}>
+            {/* We dont need to enter a path for Home component. It works according to its parent path. So - dashbord comp. */}
+            <Route index element={<Home />} />
+            {/* Absolute paths = For absolute paths, we need to add parent path to the child path, then the absolute path. */}
+            <Route path="/stock/brands" element={<Brands />} />
+            {/* Relative Paths */}
+            <Route path="firms" element={<Firms />} />
+            <Route path="products" element={<Products />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="purchases" element={<Purchases />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
