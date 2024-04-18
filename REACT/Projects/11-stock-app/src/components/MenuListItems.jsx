ab0d97836525f55a01cefeafb2fe34c3278d69e7
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-18 13:41:08 +0200

    feat: finish sidebar resposive menu

---
+++ REACT/Projects/11-stock-app/src/components/MenuListItems.jsx
@@ -0,0 +1,114 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const getIcon = (name) => `/assets/navbar/${name}.svg`;
+
+const links = [
+  {
+    title: "Dashboard",
+    url: "/stock",
+    icon: getIcon("ic_analytics"),
+  },
+
+  {
+    title: "Purchases",
+    url: "/stock/purchases",
+    icon: getIcon("purchase"),
+  },
+
+  {
+    title: "Brands",
+    url: "/stock/brands",
+    icon: getIcon("brand"),
+  },
+
+  {
+    title: "Products",
+    url: "/stock/products",
+    icon: getIcon("ic_cart"),
+  },
+
+  {
+    title: "Sales",
+    url: "/stock/sales",
+    icon: getIcon("sales"),
+  },
+
+  {
+    title: "Firms",
+    url: "/stock/firms",
+    icon: getIcon("firms"),
+  },
+];
+
+const iconStyle = {
+  color: "secondary.main",
+  borderRadius: "1rem",
+  "&:hover": {
+    backgroundColor: "secondary.main",
+    color: "white",
+  },
+  gap: 1,
+};
+const selectedStyle = {
+  backgroundColor: "secondary.second",
+  borderRadius: "1rem",
+  "&:hover": {
+    backgroundColor: "secondary.main",
+    color: "secondary.second",
+  },
+  color: "white",
+  gap: 1,
+};
+
+const MenuListItems = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      <Toolbar />
+      <List>
+        {links.map(({ title, url, icon }, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton
+              onClick={() => navigate(url)}
+              sx={pathname === url ? selectedStyle : iconStyle}
+            >
+              {/* <ListItemIcon>
+                <Box
+                  sx={{
+                    backgroundImage: `url(${icon})`,
+                    width: 24,
+                    height: 24,
+                    bgcolor: "red",
+                    backgroundPosition: "center",
+                    backgroundSize: "contain",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                />
+              </ListItemIcon> */}
+              <Box
+                sx={{
+                  width: 24,
+                  height: 24,
+                  mask: `url(${icon}) no-repeat center / contain`,
+                  bgcolor: "currentcolor",
+                }}
+              />
+              <ListItemText primary={title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default MenuListItems;
--- REACT/Projects/11-stock-app/src/pages/Dashboard.jsx
+++ REACT/Projects/11-stock-app/src/pages/Dashboard.jsx
@@ -1,54 +1,17 @@
-// import React from "react";
-// import AppBar from "@mui/material/AppBar";
-// import Box from "@mui/material/Box";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import Toolbar from "@mui/material/Toolbar";
-// import Button from "@mui/material/Button";
-// import Typography from "@mui/material/Typography";
-// import useAuthCall from "../hooks/useAuthCall";
-// import { Outlet } from "react-router-dom";
-
-// function Dashboard() {
-//   const {logout} = useAuthCall()
-//   return (
-//     <Box sx={{ display: "flex" }}>
-//       <CssBaseline />
-//       <AppBar position="fixed">
-//         <Toolbar>
-//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//             STOCK APP
-//           </Typography>
-//           <Button color="inherit" onClick={logout}>Logout</Button>
-//         </Toolbar>
-//       </AppBar>
-//       <div style={{marginTop:"10rem"}}>
-
-//       <Outlet />
-//       </div>
-//     </Box>
-//   );
-// }
-
-// export default Dashboard;
-
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Outlet } from "react-router-dom";
+import useAuthCall from "../hooks/useAuthCall";
+import Button from "@mui/material/Button";
+import LogoutIcon from "@mui/icons-material/Logout";
+import MenuListItems from "../components/MenuListItems";
 
 const drawerWidth = 240;
 
@@ -56,6 +19,7 @@ function Dashboard(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
+  const { logout } = useAuthCall();
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -72,39 +36,6 @@ function Dashboard(props) {
     }
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  // Remove this const when copying and pasting into your project.
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
@@ -116,6 +47,9 @@ function Dashboard(props) {
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
+          backgroundColor: "white",
+          color: "secondary.second",
+          borderRadius: "0.5rem",
         }}
       >
         <Toolbar>
@@ -128,9 +62,22 @@ function Dashboard(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Stock App
           </Typography>
+          <Button
+            color="inherit"
+            onClick={logout}
+            sx={{
+              gap: 1,
+              "&:hover": {
+                backgroundColor: "secondary.second",
+                color: "white",
+              },
+            }}
+          >
+            Logout <LogoutIcon />
+          </Button>
         </Toolbar>
       </AppBar>
       <Box
@@ -156,7 +103,7 @@ function Dashboard(props) {
             },
           }}
         >
-          {drawer}
+          <MenuListItems />
         </Drawer>
         <Drawer
           variant="permanent"
@@ -169,7 +116,7 @@ function Dashboard(props) {
           }}
           open
         >
-          {drawer}
+          <MenuListItems />
         </Drawer>
       </Box>
       <Box
