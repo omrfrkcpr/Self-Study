27d2e92ac5247993a8ae42715907bbbf32c385b4
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-22 14:48:45 +0200

    "Add BrandCard and BrandModal components to Brands page; use useState for modal state"
    Explanation:
    
    - The diff adds the `useState` hook to import along with `useEffect` and `useStock

---
+++ REACT/Projects/11-stock-app/src/components/ui/Cards/BrandCard.jsx
@@ -0,0 +1,73 @@
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import useStockCall from "../../../hooks/useStockCall";
+
+export default function FirmCard({
+  _id,
+  name,
+  image,
+  handleOpen,
+  setInitialState,
+}) {
+  const { deleteStockData } = useStockCall();
+  return (
+    <Card
+      sx={{
+        height: 390,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        padding: "0.5rem",
+      }}
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        sx={{ height: 140, objectFit: "contain" }}
+        component="img"
+        image={image}
+        title={name}
+      />
+      <CardActions
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <EditIcon
+          onClick={() => {
+            handleOpen();
+            setInitialState({ _id, name, image });
+          }}
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              color: "orange",
+              scale: "125%",
+            },
+          }}
+        />
+        <DeleteOutlineIcon
+          onClick={() => deleteStockData("firms", _id)}
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              color: "red",
+              scale: "125%",
+            },
+          }}
+        />
+      </CardActions>
+    </Card>
+  );
+}
--- REACT/Projects/11-stock-app/src/layouts/Brands.jsx
+++ REACT/Projects/11-stock-app/src/layouts/Brands.jsx
@@ -1,5 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import BrandCard from "../components/ui/Cards/BrandCard";
+import BrandModal from "../components/ui/Modals/BrandModal";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const Brands = () => {
   const {
@@ -10,7 +17,57 @@ const Brands = () => {
     // getBrands()
     getStockData("brands");
   }, []);
-  return <div>Brands</div>;
+
+  const { brands } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      name: "",
+      image: "",
+    });
+  };
+  const [initialState, setInitialState] = useState({
+    name: "",
+    image: "",
+  });
+  console.log("brands:", brands);
+  console.log("brands:", initialState);
+
+  return (
+    <Container>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Brands
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Brand
+      </Button>
+      <Grid container spacing={2} mt={3}>
+        {brands.map((firm) => (
+          <Grid item xs={12} md={6} lg={4} xl={3} key={firm._id}>
+            <BrandCard
+              {...firm}
+              handleOpen={handleOpen}
+              setInitialState={setInitialState}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      {open && (
+        <BrandModal
+          open={open}
+          handleClose={handleClose}
+          initialState={initialState}
+        />
+      )}
+    </Container>
+  );
 };
 
 export default Brands;
