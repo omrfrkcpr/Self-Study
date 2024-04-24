7744c7ebb478e6da6ce1f1c24564e2e993fb510d
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-24 09:51:44 +0200

    Add ProductModal and fetch product data on mount
    - Import Material UI components and hooks
    - Add state for modal open/close and handler functions
    - Fetch product, category, and brand data on component mount
    - Wrap content in

--- REACT/Projects/11-stock-app/src/layouts/Products.jsx
+++ REACT/Projects/11-stock-app/src/layouts/Products.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ProductModal from "../components/ui/Modals/ProductModal";
+import useStockCall from "../hooks/useStockCall";
 
 const Products = () => {
-  return <div>Products</div>;
+  const { getStockData } = useStockCall();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  console.log("products:", products);
+  useEffect(() => {
+    getStockData("products");
+    getStockData("categories");
+    getStockData("brands");
+  }, []);
+
+  return (
+    <Container maxWidth={"xl"}>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+      {open && <ProductModal open={open} handleClose={handleClose} />}
+    </Container>
+  );
 };
 
 export default Products;
