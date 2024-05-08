1531193279f69315ba5a6453124d3c2b31d909f0
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-05-08 12:08:15 +0200

    "Update TodoListItem to use Material UI components and accept todo prop"
    This commit updates the TodoListItem component to use Material UI ListItem, ListItemText, IconButton, and DeleteIcon components. It also now accepts

--- TYPESCRIPT/Project-Todo/src/components/TodoListItem.tsx
+++ TYPESCRIPT/Project-Todo/src/components/TodoListItem.tsx
@@ -1,5 +1,34 @@
-const TodoListItem = () => {
-  return <div>TodoListItem</div>;
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import { FC } from "react";
+
+interface ITodoListItem {
+  todo: ITodoType;
+}
+
+const TodoListItem: FC<ITodoListItem> = ({ todo }) => {
+  return (
+    <ListItem
+      key={todo.id}
+      disableGutters
+      sx={{ padding: "1rem", cursor: "pointer" }}
+      secondaryAction={
+        <IconButton
+          sx={{ "&:hover": { color: "indianred" } }}
+          aria-label="delete"
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
+    >
+      <ListItemText
+        sx={{ wordWrap: "break-word", marginRight: "1rem" }}
+        primary={todo.task}
+      />
+    </ListItem>
+  );
 };
 
 export default TodoListItem;
