a8ee7d430e0bb2d231a8d483b7c2108fe5969043
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-05-27 15:21:32 +0200

    "Remove Album.session.sql file and modify test.sqlite3 database"

--- SQL/intro/Album.session.sql
+++ SQL/intro/Album.session.sql
@@ -1,10 +1,10 @@
 -- SELECT * FROM album
 -- SELECT Country, count(Country) as countryCount FROM Customer GROUP BY Country HAVING countryCount > 1;
 
---SELECT GenreId, Name FROM Genre;
+SELECT GenreId, Name FROM Genre;
 
 --INSERT INTO Genre (GenreId, Name) VALUES (26, "Turkish Traditional Music");
 
 --UPDATE Genre SET Name = "Traditional Music" WHERE GenreId=26 --- WHERE ile belirtmek onemli, yoksa hepsi degisir
 
-DELETE Genre WHERE GenreId=26
+--DELETE FROM Genre WHERE GenreId=26
