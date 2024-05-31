8695591d97917714efe233545bd396cd22d344ee
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-05-31 12:13:02 +0200

    "Updated index.js and module.js files with various require and export examples, added dotenv dependency and updated package.json and package-lock.json files accordingly."

--- NODEJS/04-modules/index.js
+++ NODEJS/04-modules/index.js
@@ -1,6 +1,53 @@
 "use strict";
 
 console.log("module lesson - index.js");
-require("./modules/module.js");
-require("./modules/module");
-require("./modules/"); // default index.js arar
+// require("./modules/module.js");
+// require("./modules/module");
+// require("./modules/"); // default index.js arar
+
+//* Single Function
+// const testFuncFromModule = require("./modules/module");
+// testFuncFromModule();
+
+//* Multi Function - (Destructuring)
+//? Array
+// const [testFunc_A, testFunc_B, testFunc_C] = require("./modules/module");
+// testFunc_A();
+// testFunc_B();
+// testFunc_C();
+
+//? Object
+// const { testA: t_A, testB, testC } = require("./modules/module");
+// t_A();
+// testB();
+// testC();
+
+//? Shorthand
+// const {
+//   testFunction_A: test_A,
+//   testFunction_B,
+//   testFunction_C,
+// } = require("./modules/module");
+// test_A();
+// testFunction_B();
+// testFunction_C();
+
+//? Shorthand 2
+// const {
+//   testFunction_A,
+//   testFunction_B,
+//   testFunction_C,
+// } = require("./modules/module");
+
+// testFunction_A();
+// testFunction_B();
+// testFunction_C();
+
+//! buildin -- (https://www.w3schools.com/nodejs/ref_modules.asp)
+// console.log(require("module").builtinModules);
+require("http"); // first check if it exists in node_modules, then check it in global
+require("node:http"); // check directly in global
+require("dotenv").config(); // call config func from dotenv module
+// console.log(process.env);
+console.log(process.env.ENV_HOST);
+console.log(process.env.ENV_PORT);
--- NODEJS/04-modules/modules/module.js
+++ NODEJS/04-modules/modules/module.js
@@ -1,3 +1,59 @@
 "use strict";
 
 console.log("module page");
+
+//* Single Function
+// function testFunction() {
+//   console.log("this is a test function");
+// }
+
+// module.exports = testFunction;
+// module.exports = testFunction(){
+//   console.log("this is a test function");
+//};
+
+//* Multi Function
+
+// function testFunction_A() {
+//   console.log("this is a test function a");
+// }
+// function testFunction_B() {
+//   console.log("this is a test function b");
+// }
+// function testFunction_C() {
+//   console.log("this is a test function b");
+// }
+
+//? Array
+// module.exports = [testFunction_A, testFunction_B, testFunction_C];
+
+//? Object
+// module.exports = {
+//   testA: testFunction_A,
+//   testB: testFunction_B,
+//   testC: testFunction_C,
+// };
+
+//? Shorthand
+// module.exports.testFunction_A = function () {
+//   console.log("this is a test function a");
+// };
+// module.exports.testFunction_B = function () {
+//   console.log("this is a test function b");
+// };
+// module.exports.testFunction_C = function () {
+//   console.log("this is a test function b");
+// };
+
+//? Shorthand 2
+// module.exports = {
+//   testFunction_A: function () {
+//     console.log("this is a test function a");
+//   },
+//   testFunction_B: function () {
+//     console.log("this is a test function a");
+//   },
+//   testFunction_C: function () {
+//     console.log("this is a test function a");
+//   },
+// };
