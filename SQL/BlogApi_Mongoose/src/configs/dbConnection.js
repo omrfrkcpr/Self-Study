736b29db290e28ff1f305513a16e65b37b6cd2cb
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-14 09:58:09 +0200

    "Added Mongoose dependency and implemented MongoDB connection in dbConnection.js"

--- SQL/BlogApi_Mongoose/src/configs/dbConnection.js
+++ SQL/BlogApi_Mongoose/src/configs/dbConnection.js
@@ -1,1 +1,13 @@
 //* MongoDb connection
+
+const mongoose = require("mongoose");
+
+main()
+  .then(() => console.log("DB connection"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
