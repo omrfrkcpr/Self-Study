06dc9652ad828cc20593780e59baa6445ef4dcce
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-28 09:55:54 +0200

    refactor: Update Tutorial model schema and toJSON method
    This commit refactors the Tutorial model schema in the `tututorialModel.js` file. The `trype` typo is corrected to `type` for both the `title` and `description` fields. Additionally, the `timestamps` option is set to `false` to disable automatic timestamp generation. The `toJSON` method is updated to remove the `_id` field from the serialized JSON object.

--- NODEJS/projects/Tutorial_App/backend/models/tututorialModel.js
+++ NODEJS/projects/Tutorial_App/backend/models/tututorialModel.js
@@ -1,26 +1,39 @@
 "use strict";
-
 const mongoose = require("mongoose");
 
 const tutorialSchema = new mongoose.Schema(
   {
     title: {
-      trype: String,
+      type: String,
       trim: true,
       required: true,
     },
     description: {
-      trype: String,
+      type: String,
       trim: true,
       required: true,
     },
   },
   {
     collection: "tutorial",
-    timestamps: true,
+    timestamps: false,
   }
 );
 
+tutorialSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+// tutorialSchema.set("toJSON", {
+//   transform: function (doc, ret, options) {
+//     ret.id = ret._id;
+//     delete ret._id;
+//     delete ret.__v;
+//   },
+// });
 const Tutorial = mongoose.model("Tutorial", tutorialSchema);
 
 module.exports = Tutorial;
