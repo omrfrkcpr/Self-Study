61bf3ea21ce82d68b45482c26c9d3788e625c388
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-01 09:28:39 +0200

    chore: Update department.model.js to adhere to coding standards

--- NODEJS/projects/Personnel_API/src/models/department.model.js
+++ NODEJS/projects/Personnel_API/src/models/department.model.js
@@ -1,6 +1,25 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+"use strict";
+/* ------------------------------------------ */
+/*          EXPRESS - Personnel API           */
+/* ------------------------------------------ */
+
+const { mongoose } = require("../configs/dbConnection");
+
+const departmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "departments",
+  }
+);
+
+const DepartmentModel = mongoose.model("Department", departmentSchema);
+
+module.exports = DepartmentModel;
