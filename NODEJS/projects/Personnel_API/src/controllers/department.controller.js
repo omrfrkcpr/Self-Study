b8fd78d83be24febf6a5c12e996610c217fbc649
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-01 09:46:43 +0200

    feat: Refactor department controller for improved functionality
    The department.controller.js file has been refactored to improve the functionality of the department controller. Changes include:
    - Adding the DepartmentModel import
    - Updating the list function to use the DepartmentModel
    - Updating the create function to use the DepartmentModel
    - Updating the read function to use the DepartmentModel
    - Updating the update function to use the DepartmentModel
    - Updating the delete function to use the DepartmentModel
    
    These changes aim to enhance the overall functionality and maintainability of the department controller in the Personnel API.

--- NODEJS/projects/Personnel_API/src/controllers/department.controller.js
+++ NODEJS/projects/Personnel_API/src/controllers/department.controller.js
@@ -1,4 +1,55 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+"use strict";
+/* ------------------------------------------ */
+/*          EXPRESS - Personnel API           */
+/* ------------------------------------------ */
+
+const DepartmentModel = require("../models/department.model");
+
+module.exports.departmentController = {
+  list: async (req, res) => {
+    // const departments = await res.getModelList(Department)
+    const data = await res.getModelList(DepartmentModel);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Department),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await DepartmentModel.create(req.body);
+    res.status(201).send({
+      error: false,
+      message: "Department successfully created",
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await DepartmentModel.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await DepartmentModel.updateOne(
+      { _id: req.params.id },
+      req.body,
+      {
+        runValidators: true,
+      }
+    );
+    res.stauts(202).send({
+      error: false,
+      message: "Department successfully updated",
+      data,
+      newData: await DepartmentModel.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await DepartmentModel.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount > 0 ? 201 : 404).send({
+      error: !data.deletedCount,
+      message: "Department successfully deleted",
+    });
+  },
+};
