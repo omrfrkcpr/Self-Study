a8154e65d50cb9e846f0a0d1de034bb6832cdd49
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-08 11:50:06 +0200

    feat: Update authentication middleware to use 'authorization' header
    This commit updates the authentication middleware in the Pizza API to use the 'authorization' header instead of the 'authentication' header. The code change replaces the variable name and updates the comments accordingly. This change ensures consistency with industry-standard naming conventions for authorization headers.
    
    ```

--- NODEJS/projects/Pizza_API/src/middlewares/authentication.js
+++ NODEJS/projects/Pizza_API/src/middlewares/authentication.js
@@ -6,11 +6,11 @@
 const Token = require("../models/token");
 
 module.exports = async (req, res, next) => {
-  const auth = req.headers?.authentication;
+  const auth = req.headers?.authorization;
   const tokenKey = auth ? auth.split(" ") : null;
 
-  //* Example Usage: Token asdajdp1q23iejpqwojde...
   if (tokenKey && tokenKey[0] == "Token") {
+    //* "Token asdaırhıawerasd...""
     const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
       "userId"
     );
