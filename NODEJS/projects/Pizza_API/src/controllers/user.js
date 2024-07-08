a01e7bbbe2a845f8e9d01bff8281c23e0134c6fe
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-08 09:40:28 +0200

    feat: Refactor user controller to improve code structure and error handling
    This commit refactors the user controller in the Pizza API to improve code structure and error handling. It includes the following changes:
    
    - Reorganized the code to separate different CRUD operations into individual functions.
    - Added error handling for database queries and responses.
    - Updated the response format to include error and success messages.
    
    These changes aim to enhance the maintainability and reliability of the user controller in the Pizza API by improving code organization and error handling.

--- NODEJS/projects/Pizza_API/src/controllers/user.js
+++ NODEJS/projects/Pizza_API/src/controllers/user.js
@@ -1,4 +1,74 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+"use strict";
+/* ---------------------------------- */
+/*     NODEJS EXPRESS | PIZZA API     */
+/* ---------------------------------- */
+
+const User = require("../models/user");
+
+module.exports = {
+  listUsers: async (req, res) => {
+    const data = await res.getModelList(User);
+    res.status(200).send({
+      error: false,
+      defails: await res.getModelListDetails,
+      data,
+    });
+  },
+  createUser: async (req, res) => {
+    const data = await User.create(req.body);
+    res.status(201).send({
+      error: false,
+      message: "New User successfully created",
+      data,
+    });
+  },
+  readUser: async (req, res) => {
+    const data = await User.findOne({ _id: req.params.id });
+
+    if (data) {
+      res.status(200).send({
+        error: false,
+        data,
+      });
+    } else {
+      res.status(404).send({
+        error: true,
+        message: "User not found",
+      });
+    }
+  },
+  updateUser: async (req, res) => {
+    const data = await User.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+
+    if (data.modifiedCount > 0) {
+      res.status(202).send({
+        error: false,
+        message: "User successfully updated",
+        data,
+      });
+    } else {
+      res.status(404).send({
+        error: true,
+        message: "User not found",
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    const data = await User.deleteOne({ _id: req.params.id });
+    if (data.deletedCount > 0) {
+      res.status(204).send({
+        error: false,
+        message: "User successfully deleted",
+        data,
+      });
+    } else {
+      res.status(404).send({
+        error: true,
+        message: "User not found",
+        data,
+      });
+    }
+  },
+};
