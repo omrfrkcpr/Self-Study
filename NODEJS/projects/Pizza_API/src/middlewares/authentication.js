e5584861da1caa023bc0fe77cb707f0493d61dc4
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-09 10:42:51 +0200

    refactor: Update authentication middleware to support JWT access tokens
    This commit updates the authentication middleware in the Pizza API to support JWT access tokens. It includes the following changes:
    
    - Added the `jsonwebtoken` dependency to the `authentication.js` file.
    - Implemented token verification for both simple/basic tokens and JWT access tokens.
    - Updated the logic to populate the `req.user` object with the user data from the token.
    - Removed commented code related to simple/basic tokens.
    
    These changes aim to enhance the authentication mechanism in the Pizza API by adding support for JWT access tokens.

--- NODEJS/projects/Pizza_API/src/middlewares/authentication.js
+++ NODEJS/projects/Pizza_API/src/middlewares/authentication.js
@@ -4,17 +4,45 @@
 /* ---------------------------------- */
 
 const Token = require("../models/token");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   const auth = req.headers?.authorization;
   const tokenKey = auth ? auth.split(" ") : null;
 
-  if (tokenKey && tokenKey[0] == "Token") {
-    //* "Token asdaırhıawerasd...""
-    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
-      "userId"
-    );
-    req.user = tokenData ? tokenData.userId : false;
+  // if (tokenKey && tokenKey[0] == "Token") {
+  //   // "Token asdaırhıawerasd...""
+  //   const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+  //     "userId"
+  //   );
+  //   req.user = tokenData ? tokenData.userId : false;
+  // }
+
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+      // Simple/Basic/Classic Token
+      //* "Token asdaırhıawerasd...""
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      req.user = tokenData ? tokenData.userId : false;
+    } else if (tokenKey[0] == "Bearer") {
+      // JWT Access Token
+      jwt.verify(
+        tokenKey[1],
+        process.env.ACCESS_KEY,
+        function (error, accessData) {
+          if (accessData) {
+            console.log("JWT verified");
+            req.user = accessData;
+          } else {
+            console.log("JWT failed to verify:", error);
+            req.user = false;
+          }
+        }
+      );
+    }
   }
+
   next();
 };
