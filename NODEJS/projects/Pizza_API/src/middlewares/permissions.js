7dab18c048628590f4b11292a38756766a135d4a
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-09 11:42:54 +0200

    refactor: Update permissions middleware to include isAdmin check

--- NODEJS/projects/Pizza_API/src/middlewares/permissions.js
+++ NODEJS/projects/Pizza_API/src/middlewares/permissions.js
@@ -1,18 +1,27 @@
-"use strict"
+"use strict";
 
-const { CustomError } = require("../errors/customError")
+const { CustomError } = require("../errors/customError");
 
 /* ---------------------------------- */
 /*     NODEJS EXPRESS | PIZZA API     */
 /* ---------------------------------- */
 
 module.exports = {
-
-    isLogin: (req, res, next) => {
-        if (req.user && req.user.isActive) {
-            next()
-        } else {
-            throw new CustomError("NoPermission: You must login.", 403);
-        }
-    },
-}
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      throw new CustomError("NoPermission: You must login.", 403);
+    }
+  },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      throw new CustomError(
+        "NoPermission: Only admin can perform this action!",
+        403
+      );
+    }
+  },
+};
