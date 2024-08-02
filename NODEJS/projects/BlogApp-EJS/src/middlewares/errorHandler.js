d904ac063eed11f096575e510661ea7e10af5bb2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-08-02 11:37:13 +0200

    feat: Update errorHandler.js to handle API and template errors separately
    This commit updates the errorHandler.js file in the BlogApp-EJS project. It modifies the error handling logic to differentiate between API errors and template errors. If the request URL starts with "/api", the error response is sent as JSON. Otherwise, the error response is rendered using the "error" template. This change improves the error handling and provides a more consistent user experience.

--- NODEJS/projects/BlogApp-EJS/src/middlewares/errorHandler.js
+++ NODEJS/projects/BlogApp-EJS/src/middlewares/errorHandler.js
@@ -1,13 +1,27 @@
 //* Error Handler
 
-module.exports = (err,req,res,next)=>{
-    console.log(err)
-    const errorStatusCode = res?.errorStatusCode || 500;
-    res.status(errorStatusCode).send({
-      error: true,
-      status: errorStatusCode,
-      message: err.message,
-      cause: err.cause,
-      // stack: err.stack
-    });
-}
+module.exports = (err, req, res, next) => {
+  console.log(err);
+  const errorStatusCode = res?.errorStatusCode || 500;
+  // res.status(errorStatusCode).send({
+  //   error: true,
+  //   status: errorStatusCode,
+  //   message: err.message,
+  //   cause: err.cause,
+  //   // stack: err.stack
+  // });
+
+  const errorData = {
+    error: true,
+    message: err.message,
+    cause: err.cause,
+  };
+
+  if (req.url.startsWith("/api")) {
+    // vie api
+    res.status(errorStatusCode).send(errorData);
+  } else {
+    // via template
+    res.render("error", { data: errorData });
+  }
+};
