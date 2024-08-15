31e73d17b42b70b02e7fbed8a96b58c4f2c2e0ad
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-08-15 09:59:58 +0200

    feat: Update page metadata and layout in movie app
    This commit updates the page metadata and layout in the movie app. It includes the following changes:
    
    - The page metadata, including the title and description, is updated to reflect the purpose of the app.
    - The layout of the home page is improved with a new banner image and a call-to-action button.
    
    These changes enhance the user experience by providing more accurate and appealing metadata and improving the visual presentation of the home page.

--- NEXTJS/projects/movie-app-nextjs/src/app/page.js
+++ NEXTJS/projects/movie-app-nextjs/src/app/page.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Movie App</h1>
+    <main className="h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <div className="text-white text-center relative top-2/4 m-auto">
+          <h1 className="text-5xl font-[900] text-center mb-3 ">
+            Unlimited movies, TV shows, and more
+          </h1>
+          <p className="text-2xl font-[400] ">
+            Watch anywhere. Cancel anytime.
+          </p>
+          <Link href="/register">
+            <button className="btn-danger w-[250px]">Get Started</button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
